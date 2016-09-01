13cd28e9a7e46f56da3b95973d11824eba3daaa5
boggy_christopher <christopher@boggybumblebee.com>
2016-09-01 16:58:20 -0400

    Added Mockito and completed happy and exception path Unit Tests

--- src/test/java/org/codeforprinceton/twitter/TwitterSearchAgentTest.java
+++ src/test/java/org/codeforprinceton/twitter/TwitterSearchAgentTest.java
@@ -2,28 +2,50 @@ package org.codeforprinceton.twitter;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import twitter4j.Query;
+import twitter4j.QueryResult;
 import twitter4j.Status;
-import twitter4j.TwitterFactory;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
 
+import org.codeforprinceton.twitter.TwitterSearchAgent;
+
+@RunWith(MockitoJUnitRunner.class)
 public class TwitterSearchAgentTest {
 
 	private static final String SIMPLE_QUERY_STRING = "#codeforprinceton";
 
-	private static final int SIMPLE_RETURN_SIZE = 0;
+	private static final int SIMPLE_RETURN_ZERO = 0;
 
+	private static final int SIMPLE_RETURN_ONE = 1;
+
+	private static final int SIMPLE_RETURN_TEN = 10;
+
+	@Mock
+	Twitter twitter;
+
+	@InjectMocks
 	TwitterSearchAgent agent = null;
 
 	@Before
 	public void setUp() throws Exception {
 
-		agent = new TwitterSearchAgent(TwitterFactory.getSingleton());
+		agent = new TwitterSearchAgent(twitter);
 	}
 
 	@After
@@ -33,12 +55,83 @@ public class TwitterSearchAgentTest {
 	}
 
 	@Test
-	public void simpleQueryForHashtags_Test() {
+	public void simpleQueryForHashtags_TestZeroTweets() throws TwitterException {
+
+		when(twitter.search(new Query(SIMPLE_QUERY_STRING))).thenReturn(mock(QueryResult.class));
 
 		List<Status> hashtags = agent.simpleQuery(SIMPLE_QUERY_STRING);
 
 		assertNotNull("Simple Query for Hashtags should not return a null response!", hashtags);
-		assertEquals("Simple Query for Hashtags should return zero results!", SIMPLE_RETURN_SIZE, hashtags.size());
+		assertEquals("Simple Query for Hashtags should return zero results!", SIMPLE_RETURN_ZERO, hashtags.size());
+
+		verify(twitter).search(new Query(SIMPLE_QUERY_STRING));
+	}
+
+	@Test
+	public void simpleQueryForHashtags_TestOneTweet() throws TwitterException {
+
+		QueryResult queryResult = mock(QueryResult.class);
+
+		List<Status> statuses = new ArrayList<Status>();
+		statuses.add(mock(Status.class));
+
+		when(twitter.search(new Query(SIMPLE_QUERY_STRING))).thenReturn(queryResult);
+		when(queryResult.getTweets()).thenReturn(statuses);
+
+		List<Status> hashtags = agent.simpleQuery(SIMPLE_QUERY_STRING);
+
+		assertNotNull("Simple Query for Hashtags should not return a null response!", hashtags);
+		assertEquals("Simple Query for Hashtags should return one results!", SIMPLE_RETURN_ONE, hashtags.size());
+
+		verify(twitter).search(new Query(SIMPLE_QUERY_STRING));
+	}
+
+	@Test
+	public void simpleQueryForHashtags_TestTenTweets() throws TwitterException {
+
+		QueryResult queryResult = mock(QueryResult.class);
+
+		List<Status> statuses = new ArrayList<Status>();
+
+		for (int position = 0; position < SIMPLE_RETURN_TEN; position++) {
+			statuses.add(mock(Status.class));
+		}
+
+		when(twitter.search(new Query(SIMPLE_QUERY_STRING))).thenReturn(queryResult);
+		when(queryResult.getTweets()).thenReturn(statuses);
+
+		List<Status> hashtags = agent.simpleQuery(SIMPLE_QUERY_STRING);
+
+		assertNotNull("Simple Query for Hashtags should not return a null response!", hashtags);
+		assertEquals("Simple Query for Hashtags should return ten results!", SIMPLE_RETURN_TEN, hashtags.size());
+
+		verify(twitter).search(new Query(SIMPLE_QUERY_STRING));
+	}
+
+	@Test
+	public void simpleQueryForHashtags_TwitterException() throws TwitterException {
+
+		when(twitter.search(new Query(SIMPLE_QUERY_STRING))).thenThrow(TwitterException.class);
+
+		List<Status> hashtags = agent.simpleQuery(SIMPLE_QUERY_STRING);
+
+		assertNotNull("Simple Query for Hashtags should not return a null response!", hashtags);
+		assertEquals("Simple Query for Hashtags should return zero results!", SIMPLE_RETURN_ZERO, hashtags.size());
+
+		verify(twitter).search(new Query(SIMPLE_QUERY_STRING));
+	}
+
+	@Test
+	public void simpleQueryForHashtags_Null() throws TwitterException {
+
+		when(twitter.search(new Query(null))).thenThrow(TwitterException.class);
+
+		List<Status> hashtags = agent.simpleQuery(null);
+
+		assertNotNull("Simple Query for Hashtags should not return a null response!", hashtags);
+		assertEquals("Simple Query for Hashtags should return zero results!", SIMPLE_RETURN_ZERO, hashtags.size());
+
+		verify(twitter).search(new Query(null));
 	}
 
 }
